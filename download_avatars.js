446dcf11e84323acb8322bb1a2ba966476b9f96e
Kyla Palos <kyla.palos@gmail.com>
2018-08-06 18:39:08 +0000

    getting avatar_urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,28 @@
 var request = require("request");
+const secret = require("./secrets")
 var myArgs = process.argv.slice(2);
 
 console.log("Welcome to the Github Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb){
   var options = {
-    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors_url`,
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': secret.GITHUB_TOKEN
     }
   };
 
   request(options, (err, res, body) => {
-    cb(err, body);
+    var buffer = JSON.parse(body);
+    cb(err, buffer);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach((e) => {
+    console.log(e.avatar_url);
+  });
 });
 
