cc340d4af9fa337ee1d33e90e8aa6781ff7b148d
Kyla Palos <kyla.palos@gmail.com>
2018-08-06 17:55:10 +0000

    accessing contributors of repository -- denied access

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,23 @@
 var request = require("request");
+var myArgs = process.argv.slice(2);
 
 console.log("Welcome to the Github Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb){
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors_url`,
+    headers: {
+      'User-Agent': 'application/json'
+    }
+  };
+
+  request(options, (err, res, body) => {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
